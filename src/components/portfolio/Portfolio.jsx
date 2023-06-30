067d5a778dc6c46a7ea156f85c2a729b3370e371
Harsh <xyz.gmail.com>
2023-06-30 15:51:28 +0530

    Your Message

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import './portfolio.css'
 import IMG1 from '../../assets/Sc1.png'
 import IMG2 from '../../assets/portfolio2.jpg'
-import IMG3 from '../../assets/portfolio3.jpg'
+import form from '../../assets/form.png'
+import todo from '../../assets/todo.png'
 // import IMG4 from '../../assets/portfolio4.jpg'
 // import IMG5 from '../../assets/portfolio5.png'
 // import IMG6 from '../../assets/portfolio6.jpg'
@@ -14,6 +15,35 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
+
+      <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={ps1} alt=''></img>
+          </div>
+
+          <h3>My React Portfolio </h3>
+          <div className='portfolio__item-cta'>
+            
+          <a href='https://github.com/shreyas14121999/react-portfolio-website-1' className='btn' target='_blank'>GitHub</a>
+          <a href='https://shreyas-react-portfolio.netlify.app' className='btn btn-primary' target='_blank'>Live Demo</a>
+          
+          </div>
+        </article>
+
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={todo} alt=''></img>
+          </div>
+
+          <h3>Create ToDo App</h3>
+          <div className='portfolio__item-cta'>
+            
+          <a href='https://github.com/shreyas14121999/Facebook' className='btn' target='_blank'>GitHub</a>
+          <a href='https://react-todo-shreyas.netlify.app' className='btn btn-primary' target='_blank'>Live Demo</a>
+          
+          </div>
+        </article>
+
         <article className="portfolio__item">
           <div className="portfolio__item-image">
             <img src={IMG1} alt=''></img>
@@ -28,46 +58,22 @@ const Portfolio = () => {
           </div>
         </article>
 
+      
         <article className="portfolio__item">
           <div className="portfolio__item-image">
-            <img src={ps1} alt=''></img>
+            <img src={form} alt=''></img>
           </div>
 
-          <h3>This is a My React Portfolio </h3>
+          <h3>Its an Simple React Register Form</h3>
           <div className='portfolio__item-cta'>
             
-          <a href='https://github.com/shreyas14121999/react-portfolio-website-1' className='btn' target='_blank'>GitHub</a>
-          <a href='https://shreyas-react-portfolio.netlify.app/' className='btn btn-primary' target='_blank'>Live Demo</a>
-          
-          </div>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG3} alt=''></img>
-          </div>
-
-          <h3>This is a Portfolio Item Title</h3>
-          <div className='portfolio__item-cta'>
-            
-          <a href='https://github.com/shreyas14121999/Facebook' className='btn' target='_blank'>GitHub</a>
-          <a href='https://github.com/shreyas14121999/Facebook' className='btn btn-primary' target='_blank'>Live Demo</a>
+          <a href='https://github.com/shreyas14121999/Register-Form' className='btn' target='_blank'>GitHub</a>
+          <a href='https://simple-register-form.netlify.app' className='btn btn-primary' target='_blank'>Live Demo</a>
           
           </div>
         </article>
 
-        {/* <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG4} alt=''></img>
-          </div>
-
-          <h3>This is a Portfolio Item Title</h3>
-          <div className='portfolio__item-cta'>
-            
-          <a href='https://github.com/shreyas14121999/Facebook' className='btn' target='_blank'>GitHub</a>
-          <a href='https://github.com/shreyas14121999/Facebook' className='btn btn-primary' target='_blank'>Live Demo</a>
-          
-          </div>
-        </article> */}
+        
 
         {/* <article className="portfolio__item">
           <div className="portfolio__item-image">
